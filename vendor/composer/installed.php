4e7f1e6b43bcb6c87cd5b03468e8959a1f54581b
nessakodo <nessakodo@gmail.com>
2023-10-31 08:29:54 -0600

    adding composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nessakodo/nessakodo.com',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '010c89ce9a75f10a2d650fafa5817c36f11ccbec',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nessakodo/nessakodo.com' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '010c89ce9a75f10a2d650fafa5817c36f11ccbec',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
